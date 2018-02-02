1d4a7da1aeb8c2262d969cc9a9090df03a281063
Peter Zijlstra <peterz@infradead.org>
2018-02-02 10:27:00 +0100

    sched/cpufreq: Provide migration hint
    It was suggested that a migration hint might be usefull for the
    CPU-freq governors.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- include/linux/sched/cpufreq.h
+++ include/linux/sched/cpufreq.h
@@ -11,6 +11,7 @@
 #define SCHED_CPUFREQ_RT	(1U << 0)
 #define SCHED_CPUFREQ_DL	(1U << 1)
 #define SCHED_CPUFREQ_IOWAIT	(1U << 2)
+#define SCHED_CPUFREQ_MIGRATION	(1U << 3)
 
 #ifdef CONFIG_CPU_FREQ
 struct update_util_data {
